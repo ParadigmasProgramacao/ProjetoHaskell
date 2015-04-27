46e062257c6c3516f3104d3d34ae0955767ee7b4
LucianoAlmeida <mdiebr@gmail.com>
2015-04-27 08:29:20 -0300

    Creating Stadard Deviation Calc

--- Calculation/Statistics.hs
+++ Calculation/Statistics.hs
@@ -1,8 +1,23 @@
-module Statistics (averag) where
+module Calculation.Statistics
+( sumList, average, variance, stdDeviation
+) where
 
--- Calcular média
-average :: [Float] -> Float
-average [] = 0.0
-average (x:xs) = sum(x:xs) / (fromIntegral (length(x:xs)))
+-- Sum a list
+sumList :: [Float] -> Float
+sumList [] = 0
+sumList (headList:tailList) = headList + sumList tailList
 
+-- Average
+average :: Float -> Float -> Float
+average 0 totalSum = 0
+average qtdSamples totalSum = (totalSum/qtdSamples)
 
+-- Variance
+variance :: [Float] -> Float -> Float
+variance [] total = 0
+variance list total = (sumList([(i-(average total (sumList list)))^2 | i <- list]))/total
+
+-- Standard Deviation
+stdDeviation :: [Float] -> Float -> Float
+stdDeviation [] total = 0
+stdDeviation list total = sqrt ((sumList([(i-(average total (sumList list)))^2 | i <- list]))/total)
